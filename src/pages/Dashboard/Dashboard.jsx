e7ed26f20bd9026b742e900f20285c380b823258
Rawlinson Rocha <rawlinson.95@gmail.com>
2025-07-08 23:14:12 -0300

    Definido layout dashboard admin

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,31 @@
-import { logout } from "../../services/authServices";
-import { useNavigate } from "react-router-dom";
+import Menu from "../../components/Menu/Menu";
+import welcomeImage from "../../assets/undraw_welcoming_42an.svg";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
-
   return (
-    <div className="container mt-5">
-      <h1>Área Protegida</h1>
-      <button className="btn btn-danger" onClick={handleLogout}>
-        Logout
-      </button>
-    </div>
+    <>
+      <Menu />
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10 col-lg-8 text-center">
+            <div className="image-container mt-4 mb-4">
+              <img
+                src={welcomeImage}
+                alt="Ilustração de boas-vindas"
+                className="img-fluid rounded"
+                style={{
+                  width: "70%",
+                }}
+              />
+            </div>
+
+            <p className="text-muted mt-3">
+              Bem-vindo ao seu painel administrativo
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
